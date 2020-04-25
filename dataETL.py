9d089de5124d613c8e748107260411a137aa1f3b
TJonathan <jonamusiitwa@outlook.com>
2020-04-25 23:28:30 +0000

    Modified dataETL.py
    Modified the etl python file in hopes that the github actions workflow will not return a build error.

--- dataETL.py
+++ dataETL.py
@@ -1,106 +1,103 @@
-import glob
+import pandas as pd
+import numpy as np
+import requests
 import io
-import re
 from datetime import date, timedelta
 
-import numpy as np
-import pandas as pd
-import requests
+file_date = date(2020, 1, 22)
+dates = []
 
+while file_date <= date.today():
+    dates.append(file_date)
+    file_date += timedelta(days=1)
 
-def etl():
-    file_date = date(2020, 1, 22)
-    dates = []
+files = []
+for file in dates:
+    file = file.strftime("%m-%d-%Y")
+    print(file)
+    url = r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}.csv'.format(
+        file)
+    raw_string = requests.get(url).content
+    df = pd.read_csv(io.StringIO(raw_string.decode('utf-8')))
+    df['date'] = pd.to_datetime(file)
+    df.rename(columns={'Country_Region': 'Country/Region'}, inplace=True)
+    files.append(df)
 
-    while file_date <= date.today():
-        dates.append(file_date)
-        file_date += timedelta(days=1)
+df = pd.concat(files, axis=0, ignore_index=True, sort=False)
 
-    files = []
-    for file in dates:
-        file = file.strftime("%m-%d-%Y")
-        print(file)
-        url = r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}.csv'.format(
-            file)
-        raw_string = requests.get(url).content
-        df = pd.read_csv(io.StringIO(raw_string.decode('utf-8')))
-        df['date'] = pd.to_datetime(file)
-        df.rename(columns={'Country_Region': 'Country/Region'}, inplace=True)
-        files.append(df)
+global_data = df.to_csv('global_data.csv', index=False)
 
-    df = pd.concat(files, axis=0, ignore_index=True, sort=False)
+df.drop(['FIPS', 'Admin2', 'Combined_Key', '404: Not Found'],
+        axis=1,
+        inplace=True)
 
-    global_data = df.to_csv('global_data.csv', index=False)
+africa = [
+    'Algeria',
+    'Angola',
+    'Benin',
+    'Botswana',
+    'Burkina Faso',
+    'Burundi',
+    'Cabo Verde',
+    'Cameroon',
+    'Central African Republic',
+    'Chad',
+    'Comoros',
+    'Côte d’Ivoire',
+    'Congo',
+    'Djibouti',
+    'Egypt',
+    'Equatorial Guinea',
+    'Eritrea',
+    'Eswatini',
+    'Ethiopia',
+    'Gabon',
+    'Gambia',
+    'Ghana',
+    'Guinea',
+    'Guinea-Bissau',
+    'Kenya',
+    'Lesotho',
+    'Liberia',
+    'Libya',
+    'Madagascar',
+    'Malawi',
+    'Mali',
+    'Mauritania',
+    'Mauritius',
+    'Morocco',
+    'Mozambique',
+    'Namibia',
+    'Niger',
+    'Nigeria',
+    'Rwanda',
+    'Sao Tome and Principe',
+    'Senegal',
+    'Seychelles',
+    'Sierra Leone',
+    'Somalia',
+    'South Africa',
+    'South Sudan',
+    'Sudan',
+    'Tanzania',
+    'Togo',
+    'Tunisia',
+    'Uganda',
+    'Zambia',
+    'Zimbabwe',
+    'Congo (Brazzaville)',
+    'Ivory Coast',
+    'Congo (Kinshasa)',
+    'Republic of the Congo',
+    'Gambia, The',
+]
 
-    df.drop(['FIPS', 'Admin2', 'Combined_Key',
-             '404: Not Found'], axis=1, inplace=True)
+df_africa = df[df['Country/Region'].isin(africa)]
 
-    africa = ['Algeria',
-              'Angola',
-              'Benin',
-              'Botswana',
-              'Burkina Faso',
-              'Burundi',
-              'Cabo Verde',
-              'Cameroon',
-              'Central African Republic',
-              'Chad',
-              'Comoros',
-              'Côte d’Ivoire',
-              'Congo',
-              'Djibouti',
-              'Egypt',
-              'Equatorial Guinea',
-              'Eritrea',
-              'Eswatini',
-              'Ethiopia',
-              'Gabon',
-              'Gambia',
-              'Ghana',
-              'Guinea',
-              'Guinea-Bissau',
-              'Kenya',
-              'Lesotho',
-              'Liberia',
-              'Libya',
-              'Madagascar',
-              'Malawi',
-              'Mali',
-              'Mauritania',
-              'Mauritius',
-              'Morocco',
-              'Mozambique',
-              'Namibia',
-              'Niger',
-              'Nigeria',
-              'Rwanda',
-              'Sao Tome and Principe',
-              'Senegal',
-              'Seychelles',
-              'Sierra Leone',
-              'Somalia',
-              'South Africa',
-              'South Sudan',
-              'Sudan',
-              'Tanzania',
-              'Togo',
-              'Tunisia',
-              'Uganda',
-              'Zambia',
-              'Zimbabwe',
-              'Congo (Brazzaville)',
-              'Ivory Coast',
-              'Congo (Kinshasa)',
-              'Republic of the Congo',
-              'Gambia, The',
-              ]
-
-    df_africa = df[df['Country/Region'].isin(africa)]
-
-    df_africa.drop(['Province/State', 'Province_State',
+df_africa.drop(['Province/State', 'Province_State',
                     'Last Update', 'Last_Update'], axis=1, inplace=True)
 
-    df_africa = df_africa[['date',
+df_africa = df_africa[['date',
                            'Country/Region',
                            'Confirmed',
                            'Deaths',
@@ -108,26 +105,24 @@ def etl():
                            'Lat',
                            'Latitude', 'Long_', 'Longitude']]
 
-    df_africa['Confirmed'] = df_africa['Confirmed'].fillna(0).astype(int)
-    df_africa['Deaths'] = df_africa['Deaths'].fillna(0).astype(int)
-    df_africa['Recovered'] = df_africa['Recovered'].fillna(0).astype(int)
-    df_africa['Active'] = df_africa['Confirmed'] - \
-        df_africa['Deaths'] - df_africa['Recovered']
+df_africa['Confirmed'] = df_africa['Confirmed'].fillna(0).astype(int)
+df_africa['Deaths'] = df_africa['Deaths'].fillna(0).astype(int)
+df_africa['Recovered'] = df_africa['Recovered'].fillna(0).astype(int)
+df_africa['Active'] = df_africa['Confirmed'] - \
+    df_africa['Deaths'] - df_africa['Recovered']
 
-    df_africa['Lat'] = df_africa.apply(
+df_africa['Lat'] = df_africa.apply(
         lambda row: row['Latitude']if np.isnan(row['Lat']) else row['Lat'],
         axis=1
     )
 
-    df_africa['Long_'] = df_africa.apply(
+df_africa['Long_'] = df_africa.apply(
         lambda row: row['Longitude']if np.isnan(
             row['Long_']) else row['Long_'],
         axis=1
     )
-    df_africa.drop(['Latitude', 'Longitude'], axis=1, inplace=True)
-    df_africa.rename(columns={'Lat': 'Latitude',
+df_africa.drop(['Latitude', 'Longitude'], axis=1, inplace=True)
+df_africa.rename(columns={'Lat': 'Latitude',
                               'Long_': 'Longitude'}, inplace=True)
 
-    african_data = df_africa.to_csv('africa_data.csv', index=False)
-
-    return global_data, african_data
+african_data = df_africa.to_csv('africa_data.csv', index=False)
